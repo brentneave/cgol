835e41ae0ba1d326910c094f355963edd898f305
Brent Neave <brent.neave@gmail.com>
2020-04-25 21:11:12 +1200

    Faster implementation of unique

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-import { isEqual, uniqWith } from 'lodash';
+// @ts-nocheck
 
 
 export const decodeNumberInput = event => parseInt(event.target.value || 0)
 
 
-export const unique = a => uniqWith(a, isEqual);
+export const unique = a => [...new Set(a)] 
 
 
 export const cellsAreEqual = (a, b) => a.x === b.x && a.y === b.y
