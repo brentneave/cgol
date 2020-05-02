5de3d1d80198da63825d311c1bd0a187faf165d4
Brent Neave <brent.neave@gmail.com>
2020-05-02 22:29:13 +1200

    Use for loop to get neightbour count

--- src/utils/getNumberOfLiveNeighbours.js
+++ src/utils/getNumberOfLiveNeighbours.js
@@ -1,11 +1,18 @@
 // @ts-nocheck
 import getCellNeighbours from "./getCellNeighbours";
 
-const getNumberOfLiveNeighbours = ({ cells, x, y }) => 
-  getCellNeighbours({ cells, x, y }).map(
-    neighbour => cells[neighbour.x][neighbour.y]
-  ).filter(
-    cell => cell
-  ).length
+const getNumberOfLiveNeighbours = ({ cells, x, y }) => {
+  var count = 0
+  const neighbours = getCellNeighbours({ cells, x, y })
+  for(var i = 0; i< neighbours.length; i++) {
+    if(cells[neighbours[i].x][neighbours[i].y]) count ++
+  }
+  return count
+}
+  // getCellNeighbours({ cells, x, y }).map(
+  //   neighbour => cells[neighbour.x][neighbour.y]
+  // ).filter(
+  //   cell => cell
+  // ).length
 
 export default getNumberOfLiveNeighbours
